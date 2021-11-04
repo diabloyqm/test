4cbb7d8edf90d7a67446fef28131043c8dcf689e
diabloyqm <yqm2948@163.com>
2021-11-04 15:59:25 +0800

    Create data_select.py

---
+++ data_select.py
@@ -0,0 +1,13 @@
+from torchvision import datasets, transforms
+import numpy as np
+def get_mnist():
+mnist_trans = transforms.Compose([transforms.ToTensor()])
+mnist_data = datasets.MNIST(root ='../data/mnist',transform=mnist_trans)
+clients = 10
+per_clients = int(len(mnist_data) / clients)
+clients_dict, index_clients_dict = {}, [i for i in range(len(mnist_data))]
+for i in range(clients):
+    clients_dict[i] = set(np.random.choice(index_clients_dict, per_clients, replace=False))
+    index_clients_dict = list(set(index_clients_dict) - clients_dict[i])
+
+
