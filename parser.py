de6c34fedb7d6e68754abfd6b38ab3d32116a79a
diabloyqm <yqm2948@163.com>
2021-11-06 17:16:52 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,7 +2,8 @@ import argparse
 def args_parser():
     parser = argparse.ArgumentParser()
     # to be continued
-    parser.add_argument('--dataset', type=str, default='mnist',help='name of dataset')
+    parser.add_argument('--dataset', type=str, default='mnist', help='name of dataset')
+    parser.add_argument('--model', type=str, default='cnn', help='name of model')
 
     args= parser.parse_args()
     return args
