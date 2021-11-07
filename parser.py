433e62b437685b70ff2cb88e1d63f5d3bc4d5a3b
diabloyqm <yqm2948@163.com>
2021-11-07 14:20:04 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -8,8 +8,8 @@ def args_parser():
     parser.add_argument('--model', type=str, default='cnn', help='name of model')
     parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
     parser.add_argument('--optimizer', type=str, default='sgd',help='type of optimizer')
-    parser.add_argument('--num_classes', type=int, default=10, help='number of classes')
-    parser.add_argument('chan_numbers', type=int, default=1, help='number of channels of imgs')
-
+    parser.add_argument('--class_numbers', type=int, default=10, help='number of classes')
+    parser.add_argument('--chan_numbers', type=int, default=1, help='number of channels of imgs')
+    parser.add_argument('--epochs', type=int, default=10, help='number of loops you want to train' )
     args = parser.parse_args()
     return args
